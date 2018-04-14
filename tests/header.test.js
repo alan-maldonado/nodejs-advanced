8222f275db01e1a794fe7a0a4eead85493725968
alan-maldonado <alan.maldonado@wizeline.com>
2018-04-14 00:11:59 -0500

    ✨ Authenticate user in testing mode

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,19 +1,54 @@
 const puppeteer = require('puppeteer');
 
-test('Adds two numbers', () => {
-  const sum = 1 + 2;
+let browser, page;
 
-  expect(sum).toEqual(3);
-});
-
-test('We can launch a browser', async () => {
-  const browser = await puppeteer.launch({
-    headless: false
+describe('Header', () => {
+  beforeEach(async () => {
+    browser = await puppeteer.launch({
+      headless: false
+    });
+    page = await browser.newPage();
+    await page.goto('localhost:3000');
   });
-  const page = await browser.newPage();
-  await page.goto('localhost:3000');
 
-  const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+  afterEach(async () => {
+    // await browser.close();
+  });
 
-  expect(text).toEqual('Blogster');
+  test('the header has the correct text', async () => {
+    const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+    expect(text).toEqual('Blogster');
+  });
+
+  test('clicking login fires oauth flow', async () => {
+    await page.click('.right a');
+
+    const url = await page.url();
+    expect(url).toMatch(/accounts\.google\.com/);
+  });
+
+  test.only('when signed in, shows logout button', async () => {
+    const id = '5ad026a61840c1682961ee6f';
+
+    const Buffer = require('safe-buffer').Buffer;
+    const sessionObject = {
+      passport: {
+        user: id
+      }
+    };
+    const sessionString = Buffer.from(
+      JSON.stringify(sessionObject)
+    ).toString('base64');
+
+    const Keygrip = require('keygrip');
+    const keys = require('../config/keys');
+    const keygrip = new Keygrip([keys.cookieKey]);
+    const sig = keygrip.sign('session=' + sessionString);
+
+    await page.setCookie({ name: 'session', value: sessionString });
+    await page.setCookie({ name: 'session.sig', value: sig });
+
+    await page.goto('localhost:3000');
+  });
 });
+
