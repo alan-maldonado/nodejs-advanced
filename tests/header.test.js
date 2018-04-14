1cd3451cb5360282d3cc22b6a62de60a2e73546e
alan-maldonado <alan.maldonado@wizeline.com>
2018-04-14 15:49:02 -0500

    ✨ Adding sessionFactory

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+const sessionFactory = require('./factories/sessionFactory');
 
 let browser, page;
 
@@ -27,27 +28,12 @@ describe('Header', () => {
     expect(url).toMatch(/accounts\.google\.com/);
   });
 
-  test.only('when signed in, shows logout button', async () => {
+  test('when signed in, shows logout button', async () => {
     const id = '5ad026a61840c1682961ee6f';
+    const { session, sig } = sessionFactory();
 
-    const Buffer = require('safe-buffer').Buffer;
-    const sessionObject = {
-      passport: {
-        user: id
-      }
-    };
-    const sessionString = Buffer.from(
-      JSON.stringify(sessionObject)
-    ).toString('base64');
-
-    const Keygrip = require('keygrip');
-    const keys = require('../config/keys');
-    const keygrip = new Keygrip([keys.cookieKey]);
-    const sig = keygrip.sign('session=' + sessionString);
-
-    await page.setCookie({ name: 'session', value: sessionString });
+    await page.setCookie({ name: 'session', value: session });
     await page.setCookie({ name: 'session.sig', value: sig });
-
     await page.goto('localhost:3000');
     await page.waitFor('a[href="/auth/logout"]');
 
