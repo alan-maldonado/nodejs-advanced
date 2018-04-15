4f13a07938df6ed68bc142d0eb3f8ebce6418c98
alan-maldonado <alan.maldonado@wizeline.com>
2018-04-14 20:54:39 -0500

    ♻️ Refactoring GET and POST

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -64,36 +64,15 @@ describe('Blogs', () => {
 
   describe('When not logged in', async () => {
     test('user cannot create blog posts', async () => {
-      const result = await page.evaluate(
-        () => {
-          return fetch('/api/blogs', {
-            method: 'POST',
-            credentials: 'same-origin',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-              title: 'My title',
-              content: 'My content'
-            })
-          }).then(res => res.json());
-        }
-      );
+      const result = await page.post('/api/blogs', {
+        title: 'T',
+        content: 'C'
+      });
       expect(result).toEqual({ error: 'You must log in!'});
     });
 
     test('user cannot get a list of blogs', async () => {
-      const result = await page.evaluate(
-        () => {
-          return fetch('/api/blogs', {
-            method: 'GET',
-            credentials: 'same-origin',
-            headers: {
-              'Content-Type': 'application/json'
-            }
-          }).then(res => res.json());
-        }
-      );
+      const result = await page.get('/api/blogs');
       expect(result).toEqual({ error: 'You must log in!'});
     });
   });
